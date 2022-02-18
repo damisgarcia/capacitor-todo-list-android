830f975fc2461799b277a6d19144b097953d0494
Damis Garcia <damisgarcia@gmail.com>
2022-02-18 12:51:51 -0300

    install capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.todolist',
+  appName: 'capacitor-todo-list-android',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/views/Geolocation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Geolocation</h1>
+    <p>Your location is:</p>
+    <p>Latitude: {{ loc.lat }}</p>
+    <p>Longitude: {{ loc.long }}</p>
+
+    <button @click="getCurrentPosition">Get Current Location</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { Geolocation } from "@capacitor/geolocation";
+const loc = ref<{
+  lat: null | number;
+  long: null | number;
+}>({
+  lat: null,
+  long: null,
+});
+
+async function getCurrentPosition() {
+  const pos = await Geolocation.getCurrentPosition();
+  loc.value = {
+    lat: pos.coords.latitude,
+    long: pos.coords.longitude,
+  };
+};
+</script>
